394c92a77261cd9360df40ea4eed1c1386b03d94
Vitalii Kotenko <caunt.official@gmail.com>
2025-11-03 22:41:52 +0200

    fix(entrypoint): sorted program arguments

--- src/SupercellProxy.Playground/EntryPoint.cs
+++ src/SupercellProxy.Playground/EntryPoint.cs
@@ -1,9 +1,11 @@
 ﻿using SupercellProxy.Playground.Network;
 
 var upstreamHost = args.Length > 0 ? args[0] : "game.haydaygame.com";
-var listenPort = args.Length > 1 && int.TryParse(args[1], out var lp) ? lp : 9339;
-var upstreamPort = args.Length > 2 && int.TryParse(args[2], out var up) ? up : 9339;
+var upstreamPort = args.Length > 1 && int.TryParse(args[1], out var up) ? up : 9339;
+var listenPort = args.Length > 2 && int.TryParse(args[2], out var lp) ? lp : 9339;
 
-var proxy = new Proxy(upstreamHost, upstreamPort, listenPort);
 var client = new Client(upstreamHost, upstreamPort);
+var proxy = new Proxy(upstreamHost, upstreamPort, listenPort);
+
+await client.RunAsync();
 // await proxy.RunAsync();
